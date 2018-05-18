375a65bbdadd17ebacc626b5a632cec2679bd867
Nicholas Bollweg <nick.bollweg@gmail.com>
2018-05-18 01:28:45 -0400

    ...from bin

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -5,6 +5,6 @@ cargo build --release --verbose
 IF ERRORLEVEL 1 EXIT /B 1
 cargo install --bin %PKG_NAME% --root %PREFIX%
 IF ERRORLEVEL 1 EXIT /B 1
-MOVE %PREFIX%\%PKG_NAME%.exe %SCRIPTS%\%PKG_NAME%.exe
+MOVE %PREFIX%\bin\%PKG_NAME%.exe %SCRIPTS%\%PKG_NAME%.exe
 IF ERRORLEVEL 1 EXIT /B 1
 DEL /F /Q %PREFIX%\.crates.toml
