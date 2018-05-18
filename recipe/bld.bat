6f3f1577c9f4c148b2dd2da29a6bdac5c43e2069
Nicholas Bollweg <nick.bollweg@gmail.com>
2018-05-17 20:20:47 -0400

    can't move to what doesn't exist

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -5,6 +5,7 @@ cargo build --release --verbose
 IF ERRORLEVEL 1 EXIT /B 1
 cargo install --bin %PKG_NAME% --root %PREFIX%
 IF ERRORLEVEL 1 EXIT /B 1
+MKDIR %LIBRARY_BIN%
 MOVE %PREFIX%\%PKG_NAME%.exe %LIBRARY_BIN%\
 IF ERRORLEVEL 1 EXIT /B 1
 DEL /F /Q %PREFIX%\.crates.toml
