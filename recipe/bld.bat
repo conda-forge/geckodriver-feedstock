07b07d376565bfbbf96a68787c1ad68aac14f02c
Nicholas Bollweg <nick.bollweg@gmail.com>
2019-09-10 18:20:49 -0400

    #9: try newer 'cargo install' invocation

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,7 +3,7 @@ rustc -V
 cargo -V
 cargo build --release --verbose
 IF ERRORLEVEL 1 EXIT /B 1
-cargo install --bin %PKG_NAME% --root %PREFIX%
+cargo install --path %PKG_NAME% --root %PREFIX%
 IF ERRORLEVEL 1 EXIT /B 1
 MOVE %PREFIX%\bin\%PKG_NAME%.exe %SCRIPTS%\%PKG_NAME%.exe
 IF ERRORLEVEL 1 EXIT /B 1
