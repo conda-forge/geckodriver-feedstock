73ec254a05015d18b86a462a0d8a4b16844a44d4
Nicholas Bollweg <nick.bollweg@gmail.com>
2019-09-10 20:54:39 -0400

    #9: check some more paths

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,8 +3,9 @@ rustc -V
 cargo -V
 cargo build --release --verbose
 IF ERRORLEVEL 1 EXIT /B 1
-cargo install --path . --root %PREFIX%
+cargo install --path . --root "%PREFIX%"
 IF ERRORLEVEL 1 EXIT /B 1
+DIR %PREFIX%\bin
 MOVE %PREFIX%\bin\%PKG_NAME%.exe %SCRIPTS%\%PKG_NAME%.exe
 IF ERRORLEVEL 1 EXIT /B 1
 DEL /F /Q %PREFIX%\.crates.toml
