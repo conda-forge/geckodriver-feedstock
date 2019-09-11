912650d2fe9a9ff544ba2622ce3bc1850f751a6f
Nicholas Bollweg <nick.bollweg@gmail.com>
2019-09-10 21:30:09 -0400

    #9: cargo (ha) cult some more from ripgrep-feedstock

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,11 +1,12 @@
 @ECHO ON
 rustc -V
 cargo -V
-cargo build --release --verbose
-IF ERRORLEVEL 1 EXIT /B 1
-cargo install --path . --root "%PREFIX%"
-IF ERRORLEVEL 1 EXIT /B 1
-DIR %PREFIX%\bin
-MOVE %PREFIX%\bin\%PKG_NAME%.exe %SCRIPTS%\%PKG_NAME%.exe
-IF ERRORLEVEL 1 EXIT /B 1
-DEL /F /Q %PREFIX%\.crates.toml
+cargo build --release --verbose                   || goto :error
+cargo install --root "%PREFIX%" --path .          || goto :error
+if not exist "%SCRIPTS%" md "%SCRIPTS%"           || goto :error
+move "%PREFIX%\bin\geckodriver.exe" "%SCRIPTS%"   || goto :error
+del /F /Q "%PREFIX%\.crates.toml"
+
+:error
+echo Failed with error #%errorlevel%.
+exit 1
--- recipe/build.sh
+++ recipe/build.sh
@@ -4,11 +4,12 @@ rustc -V
 cargo -V
 
 if [ $(uname) = Darwin ] ; then
-  export RUSTFLAGS="-C link-args=-Wl,-rpath,$PREFIX/lib"
+  export RUSTFLAGS="-C link-args=-Wl,-rpath,${PREFIX}/lib"
 else
-  export RUSTFLAGS="-C link-args=-Wl,-rpath-link,$PREFIX/lib"
+  export RUSTFLAGS="-C link-args=-Wl,-rpath-link,${PREFIX}/lib"
 fi
 
 cargo build --release --verbose
-cargo install --bin $PKG_NAME --root $PREFIX
-rm -f $PREFIX/.crates.toml
+cargo install --bin "${PKG_NAME}" --root "${PREFIX}"
+
+rm -f "${PREFIX}/.crates.toml"
