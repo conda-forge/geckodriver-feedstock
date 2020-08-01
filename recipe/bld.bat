2889d51d545c14f518deaaaf182823c20e57d703
Nicholas Bollweg <nick.bollweg@gmail.com>
2020-08-01 17:03:24 -0400

    first batch of licenses, use pytest

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,22 +2,12 @@
 rustc -V
 cargo -V
 
-cd testing\geckodriver
-
 :: Install cargo-license
 set CARGO_HOME=%BUILD_PREFIX%\cargo
 mkdir %CARGO_HOME%
 icacls %CARGO_HOME% /grant Users:F
 
-:: Don't check these on windows because slow, and weird build dep errors
-::
-:: cargo install cargo-license || goto :error
-::
-:: :: Check that all downstream libraries licenses are present
-:: set PATH=%PATH%;%CARGO_HOME%\bin
-:: cargo-license --json > dependencies.json || goto :error
-:: type dependencies.json || goto :error
-:: python %RECIPE_DIR%\check_licenses.py || goto :error
+cd testing\geckodriver
 
 cargo build --release --verbose                   || goto :error
 cargo install --root "%PREFIX%" --path .          || goto :error
--- recipe/build.sh
+++ recipe/build.sh
@@ -3,6 +3,9 @@ set -ex
 rustc -V
 cargo -V
 
+export CARGO_HOME="$BUILD_PREFIX/cargo"
+mkdir $CARGO_HOME
+
 if [ $(uname) = Darwin ] ; then
   export RUSTFLAGS="-C link-args=-Wl,-rpath,${PREFIX}/lib"
 else
@@ -10,18 +13,15 @@ else
 fi
 
 # Install cargo-license
-export CARGO_HOME="$BUILD_PREFIX/cargo"
-mkdir $CARGO_HOME
 cargo install cargo-license
 
-# Check that all downstream libraries licenses are present
-export PATH=$PATH:$CARGO_HOME/bins
-cargo-license --json > dependencies.json
-cat dependencies.json
-
-python $RECIPE_DIR/check_licenses.py
+# this is a little awkward, but saves the lengthy build
+pushd $RECIPE_DIR
+pytest -svv test_licenses.py
+popd
 
 cd testing/geckodriver
+
 cargo build --release --verbose
 cargo install --root "${PREFIX}" --path .
 
