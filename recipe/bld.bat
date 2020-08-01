429405705326556979eb678c4dc2f68d627c0f7f
Nicholas Bollweg <nick.bollweg@gmail.com>
2020-08-01 09:16:58 -0400

    start the license slog

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,9 +1,27 @@
 @ECHO ON
 rustc -V
 cargo -V
+
 cd testing\geckodriver
+
+:: Install cargo-license
+set CARGO_HOME=%BUILD_PREFIX%\cargo
+mkdir %CARGO_HOME%
+icacls %CARGO_HOME% /grant Users:F
+
+:: Don't check these on windows because slow, and weird build dep errors
+::
+:: cargo install cargo-license || goto :error
+::
+:: :: Check that all downstream libraries licenses are present
+:: set PATH=%PATH%;%CARGO_HOME%\bin
+:: cargo-license --json > dependencies.json || goto :error
+:: type dependencies.json || goto :error
+:: python %RECIPE_DIR%\check_licenses.py || goto :error
+
 cargo build --release --verbose                   || goto :error
 cargo install --root "%PREFIX%" --path .          || goto :error
+
 if not exist "%SCRIPTS%" md "%SCRIPTS%"           || goto :error
 move "%PREFIX%\bin\geckodriver.exe" "%SCRIPTS%"   || goto :error
 del /F /Q "%PREFIX%\.crates.toml"
--- recipe/build.sh
+++ recipe/build.sh
@@ -9,6 +9,18 @@ else
   export RUSTFLAGS="-C link-args=-Wl,-rpath-link,${PREFIX}/lib"
 fi
 
+# Install cargo-license
+export CARGO_HOME="$BUILD_PREFIX/cargo"
+mkdir $CARGO_HOME
+cargo install cargo-license
+
+# Check that all downstream libraries licenses are present
+export PATH=$PATH:$CARGO_HOME/bins
+cargo-license --json > dependencies.json
+cat dependencies.json
+
+python $RECIPE_DIR/check_licenses.py
+
 cd testing/geckodriver
 cargo build --release --verbose
 cargo install --root "${PREFIX}" --path .
