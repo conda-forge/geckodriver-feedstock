8343b48fc8fd4ec339102c87255b8f6469617e92
Nicholas Bollweg <nick.bollweg@gmail.com>
2025-02-25 07:36:01 -0600

    update test

--- recipe/test_selenium.py
+++ recipe/test_selenium.py
@@ -1,13 +1,12 @@
-""" test whether firefox is viable with geckodriver and selenium
+"""test whether firefox is viable with geckodriver and selenium
 
-    Adapted from
-    https://github.com/conda-forge/firefox-feedstock/blob/master/recipe/run_test.py
+Adapted from
+https://github.com/conda-forge/firefox-feedstock/blob/master/recipe/run_test.py
 """
+
+from __future__ import annotations
 import sys
 import os
-import subprocess
-import traceback
-import json
 import re
 import time
 from pathlib import Path
@@ -16,6 +15,7 @@ from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+from textwrap import indent
 
 import pytest
 
@@ -29,20 +29,21 @@ if os.environ["PKG_NAME"] == "firefox":
         r"""\s*</td>"""
     )
 
+
 @pytest.fixture
-def binary_paths():
+def binary_paths() -> tuple[Path, Path]:
     plat = sys.platform.lower()
     if "win32" in plat:
         firefox = Path(os.environ["LIBRARY_BIN"]) / "firefox.exe"
         geckodriver = Path(os.environ["SCRIPTS"]) / "geckodriver.exe"
     else:
-        geckodriver = Path(sys.prefix) / "bin" / "geckodriver"
-        app_dir = Path(sys.prefix) / "bin" / "FirefoxApp"
+        geckodriver = Path(sys.prefix) / "bin/geckodriver"
+        app_dir = Path(sys.prefix) / "bin/FirefoxApp"
 
         if "linux" in plat:
             firefox = app_dir / "firefox"
         else:
-            firefox = app_dir / "Contents" / "MacOS" / "firefox"
+            firefox = app_dir / "Contents/MacOS/firefox"
 
     assert firefox.exists()
     assert geckodriver.exists()
@@ -51,7 +52,7 @@ def binary_paths():
 
 
 @pytest.fixture
-def driver(tmp_path, binary_paths):
+def driver(tmp_path: Path, binary_paths: tuple[Path, Path]) -> webdriver.Firefox:
     firefox, geckodriver = binary_paths
     log = tmp_path / "geckodriver.log"
 
@@ -59,13 +60,12 @@ def driver(tmp_path, binary_paths):
     options.headless = True
     options.binary = FirefoxBinary(str(firefox))
 
-
     service = Service(
         executable_path=str(geckodriver),
         service_args=["--log", "trace"],
         # https://github.com/SeleniumHQ/seleniumhq.github.io/commit/c11605ec062e49b2cbf1a35ddca0bf78224cc75f
         log_path=str(log),
-        log_output=str(log)
+        log_output=str(log),
     )
 
     driver = webdriver.Firefox(options=options, service=service)
@@ -76,21 +76,25 @@ def driver(tmp_path, binary_paths):
 
     print(
         "BEGIN geckodriver.log\n\n"
-        f"""{log.read_text(encoding="utf-8")}"""
+        f"""{indent(log.read_text(encoding="utf-8"), "\t")}"""
         "\n\nEND geckdriver.log"
     )
 
 
-@pytest.mark.parametrize("thing,url,expected_re", [
-    ["license", "about:license", LICENSE_CANARY],
-    ["support", "about:support", SUPPORT_CANARY],
-])
-def test_page(thing, url, expected_re, tmp_path, driver):
+@pytest.mark.parametrize(
+    "thing,url,expected_re",
+    [
+        ["license", "about:license", LICENSE_CANARY],
+        ["support", "about:support", SUPPORT_CANARY],
+    ],
+)
+def test_page(
+    thing: str, url: str, expected_re: str, tmp_path: Path, driver: webdriver.Firefox
+) -> None:
     html = tmp_path / f"{thing}.html"
     png = tmp_path / f"{thing}.png"
 
     print(f"checking {url} for `{expected_re}`...")
-    errors = []
     driver.get(url)
 
     for i in range(3):
